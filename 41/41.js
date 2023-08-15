f28150f163710def6e01a8821c3f69770ce1cf6a
9676Yaroslav9676 <yaroslavrokitskyi@gmail.com>
2023-08-15 15:01:06 +0300

    add

--- 41/41.js
+++ 41/41.js
@@ -1,10 +1,10 @@
 let arr = ["a", "b", "c", "d"];
-let size = 2;
+let size = 3;
 let result = [];
 let count = 0;
+let value = [];
 
 for (let i = 0; i < arr.length; i++) {
-  let value = [];
   if (count < size) {
     value.push(arr[i]);
     count++;
@@ -12,7 +12,12 @@ for (let i = 0; i < arr.length; i++) {
 
   if (count == size) {
     result.push(value);
+    value = [];
     count = 0;
   }
 }
+if (value) {
+  result.push(value);
+}
+
 console.log(result);
